06e63c6fb3c4f1014c853d2928f507d5748588b7
Shomookh Alharbi <shomookh1900@hotmail.com>
2024-06-10 07:50:34 +0300

    object created + killed (day10)

--- src/main/java/org/example/services/SessionService.java
+++ src/main/java/org/example/services/SessionService.java
@@ -1,5 +1,7 @@
 package org.example.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 
@@ -13,4 +15,15 @@ public class SessionService implements Serializable {
     public int getCount() {
         return ++count;
     }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("object created");
+    }
+    @PreDestroy
+    public void killed(){
+        System.out.println("object killed");
+    }
+
+
 }
